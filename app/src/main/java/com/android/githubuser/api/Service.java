58c913c215ba7cc8b9c4b07d80097c8d077b73c7
yuvarajkarthi <yuvak250198@gmail.com>
2018-05-07 13:28:48 +0530

    Update Service.java

--- app/src/main/java/com/android/githubuser/api/Service.java
+++ app/src/main/java/com/android/githubuser/api/Service.java
@@ -6,9 +6,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
-/**
- * Created by delaroy on 3/22/17.
- */
 public interface Service {
   //  @GET("/search/users?q=language:{lang}+location:{loc}")
 //    Call<ItemResponse> getItems(@Query("lang") String lang, @Query("loc") String loc);
